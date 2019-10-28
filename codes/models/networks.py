52b12777e879acbb294927ff000d3a6853b22478
Zhou Huanxiang <zhouhx.cn@gmail.com>
2019-10-28 14:28:25 +0800

    add EDVR_woDCN & networkD_w_patch

--- codes/models/networks.py
+++ codes/models/networks.py
@@ -1,9 +1,11 @@
 import torch
+import torch.nn as nn
 import models.archs.SRResNet_arch as SRResNet_arch
 import models.archs.discriminator_vgg_arch as SRGAN_arch
+import models.archs.discriminator_patch_arch as SRGAN_patch_arch
 import models.archs.RRDBNet_arch as RRDBNet_arch
-import models.archs.EDVR_arch as EDVR_arch
-
+# import models.archs.EDVR_arch as EDVR_arch
+import functools
 
 # Generator
 def define_G(opt):
@@ -19,6 +21,14 @@ def define_G(opt):
                                     nf=opt_net['nf'], nb=opt_net['nb'])
     # video restoration
     elif which_model == 'EDVR':
+        import models.archs.EDVR_arch as EDVR_arch
+        netG = EDVR_arch.EDVR(nf=opt_net['nf'], nframes=opt_net['nframes'],
+                              groups=opt_net['groups'], front_RBs=opt_net['front_RBs'],
+                              back_RBs=opt_net['back_RBs'], center=opt_net['center'],
+                              predeblur=opt_net['predeblur'], HR_in=opt_net['HR_in'],
+                              w_TSA=opt_net['w_TSA'])
+    elif which_model == 'EDVR_woDCN':
+        import models.archs.EDVR_woDCN_arch as EDVR_arch
         netG = EDVR_arch.EDVR(nf=opt_net['nf'], nframes=opt_net['nframes'],
                               groups=opt_net['groups'], front_RBs=opt_net['front_RBs'],
                               back_RBs=opt_net['back_RBs'], center=opt_net['center'],
@@ -29,6 +39,28 @@ def define_G(opt):
 
     return netG
 
+class Identity(nn.Module):
+    def forward(self, x):
+        return x
+
+def get_norm_layer(norm_type='instance'):
+    """Return a normalization layer
+
+    Parameters:
+        norm_type (str) -- the name of the normalization layer: batch | instance | none
+
+    For BatchNorm, we use learnable affine parameters and track running statistics (mean/stddev).
+    For InstanceNorm, we do not use learnable affine parameters. We do not track running statistics.
+    """
+    if norm_type == 'batch':
+        norm_layer = functools.partial(nn.BatchNorm2d, affine=True, track_running_stats=True)
+    elif norm_type == 'instance':
+        norm_layer = functools.partial(nn.InstanceNorm2d, affine=False, track_running_stats=False)
+    elif norm_type == 'none':
+        norm_layer = lambda x: Identity()
+    else:
+        raise NotImplementedError('normalization layer [%s] is not found' % norm_type)
+    return norm_layer
 
 # Discriminator
 def define_D(opt):
@@ -37,6 +69,9 @@ def define_D(opt):
 
     if which_model == 'discriminator_vgg_128':
         netD = SRGAN_arch.Discriminator_VGG_128(in_nc=opt_net['in_nc'], nf=opt_net['nf'])
+    elif which_model == 'discriminator_patch':
+        norm_layer = get_norm_layer(norm_type=opt_net['Instance'])
+        netD = SRGAN_patch_arch.NLayerDiscriminator(input_nc=opt_net['in_nc'], ndf=opt_net['nf'], n_layers=3, norm_layer=norm_layer)
     else:
         raise NotImplementedError('Discriminator model [{:s}] not recognized'.format(which_model))
     return netD
